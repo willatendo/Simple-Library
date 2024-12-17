e0c3b0631952aedc4b1a5be28325a3e6216c54af
Willatendo <williamjswindler@gmail.com>
2024-12-17 16:38:17 -0700

    Update CommonConfigRegister.java

--- common/src/main/java/willatendo/simplelibrary/server/CommonConfigRegister.java
+++ common/src/main/java/willatendo/simplelibrary/server/CommonConfigRegister.java
@@ -1,13 +1,13 @@
 package willatendo.simplelibrary.server;
 
-import org.apache.commons.compress.utils.Lists;
 import willatendo.simplelibrary.common.config.ConfigType;
 import willatendo.simplelibrary.common.config.SimpleConfig;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public final class CommonConfigRegister {
-    private static final List<SimpleConfig> COMMON_CONFIGS = Lists.newArrayList();
+    private static final List<SimpleConfig> COMMON_CONFIGS = new ArrayList<>();
 
     public static SimpleConfig createCommonConfig(String name) {
         SimpleConfig simpleConfig = new SimpleConfig(name, ConfigType.COMMON);
